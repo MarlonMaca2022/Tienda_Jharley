2d145397d77f426d92f1d921eb664f641d44e551
manurtdev <manurita0103@gmail.com>
2026-03-03 21:14:39 -0500

    Avance LogIn

--- frontend-apicrud/js/login.js
+++ frontend-apicrud/js/login.js
@@ -1,10 +1,57 @@
 // variables globales
 const d = document;
-let userinput = d.getElementById("usuarioForm");
+let userInput = d.getElementById("usuarioForm");
 let passInput = d.getElementById("contraForm");
 let btnLogin = d.querySelector(".btnLogin");
 
 // evento click del boton login
 btnLogin.addEventListener("click",  () => {
-    alert("Voy bien chidori "+ userinput.value + " " + passInput.value);
+    //alert("Voy bien chidori "+ userinput.value + " " + passInput.value);
+    let dataForm = getData();
+    sendData(dataForm);
 });
+
+//Validación del formulario
+//Obtener datos del formulario
+
+let getData = ()=>{
+    //validar formulario
+    let user;
+    if(userInput.value && passInput.value){
+       user = {
+        usuario: userInput.value,
+        contrasena: passInput.value
+       }
+       userInput.value="";
+       passInput.value="";
+    }else{
+        alert("Debe completar ambos campos")
+    }
+    console.log(user);
+    return user;
+};
+
+//funcion para recibir los datos
+//realizar la peticion al servidor
+
+let sendData = async  (data)=>{
+    let url = "http://localhost:3000/api/login";
+
+    try {
+        let respuesta = await fetch(url, {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify(data)
+        });
+        let userLogin = respuesta.json();
+        console.log("El usuario es: "+userLogin);
+    
+    } catch (error) {
+        console.log(error);
+    }
+
+   
+
+};
